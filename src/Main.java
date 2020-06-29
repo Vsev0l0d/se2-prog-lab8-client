d2384197705ea260e0ac3d1a8d1f48e5ae2a7707
Vsevolod <vsevolod628@gmail.com>
2020-06-29 13:48:48 +0300

    музяка

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-import Interfaces.ConsoleManager;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.media.*;
 import javafx.stage.Stage;
 
-import java.io.*;
+import java.net.URL;
 
 public class Main extends Application {
-    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+    public static void main(String[] args) {
         launch(args);
     }
 
@@ -20,5 +18,12 @@ public class Main extends Application {
         primaryStage.setTitle("StudyGroupProject. Авторизация.");
         primaryStage.setScene(new Scene(root, 350, 405));
         primaryStage.show();
+        
+        URL music = getClass().getResource("GUI/Music/HiMark.mp3");
+        Media media = new Media(music.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        Thread.sleep(100);
+        mediaPlayer.play();
     }
 }
